3a471ef8636668beaf975e411b90aa56ef0ca01f
VeryHardBit <yanothaic8@gmaill.com>
2022-08-29 15:26:51 +0700

    Added Grader for passive checking

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Bandit
 Abstract Data types is quite insane here
+
+Grader with difflib is also provided
--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-    int n;
-    scanf("%d",&n);
-    printf("%d\n",n+5);
-    return 0;
-}
